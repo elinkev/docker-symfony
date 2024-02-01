21dc08f350df1930d55da5cb0c45e6878d82e252
elinkev <linkeviciusedgaras@gmail.com>
2024-02-01 20:48:04 +0200

    Update entrypoint.sh

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+# PHP Coding Standards Fixer
 PHPCSPATH=./tools/php-cs-fixer
 if [ ! -d "$PHPCSPATH" ]; then
     echo "Installing PHP Coding Standards Fixer"
     mkdir -p "$PHPCSPATH"
     composer require --working-dir="$PHPCSPATH" friendsofphp/php-cs-fixer --no-interaction
+fi
+
+# Pre commit hook
+PRECOMMITHOOKPATH=./tools/pre-commit
+if [ ! -e ".git/hooks/pre-commit" ]; then
+    echo "Copying pre-commit hook into .git dir"
+    cp "$PRECOMMITHOOKPATH" .git/hooks/pre-commit
+    chmod +x .git/hooks/pre-commit
 fi
 
 exec "$@"
