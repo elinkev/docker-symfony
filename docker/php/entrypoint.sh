8fa927ffa99d03cdc43385a64163b09c71ed4994
Edgaras Linkevicius <linkeviciusedgaras@gmail.com>
2024-02-04 12:47:17 +0200

    replaced how symfony is being installed

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -3,7 +3,9 @@
 # Installing symfony if /backend directory is empty
 if [ -z "$(ls -A)" ]; then
     echo "Installing symfony"
-    symfony new . --version="7.0.*" &&
+    set -x
+    # Install symfony with composer, cli initialized git which is not needed in this dir
+    symfony composer create-project symfony/skeleton:"7.0.*" . &&
     symfony composer config extra.symfony.allow-contrib true &&
     symfony composer require symfony/apache-pack &&
     symfony composer require symfony/orm-pack &&
@@ -11,17 +13,19 @@ if [ -z "$(ls -A)" ]; then
     symfony composer require symfony/mailer &&
     symfony composer require phpstan/phpstan --dev &&
     symfony composer require phpunit/phpunit ^11 --dev &&
-    rm -rf .git
+    set +x
 fi
 
 # Installing phpcs if /tools/php-cs-fixer dir doesnt exist
 PHPCSPATH=./tools/php-cs-fixer
 if [ ! -d "$PHPCSPATH" ]; then
+    set -x
     echo "Installing PHP Coding Standards Fixer"
     mkdir -p "$PHPCSPATH" &&
     symfony composer require --working-dir="$PHPCSPATH" friendsofphp/php-cs-fixer --no-interaction &&
     mv /usr/local/bin/php-cs-fixer.sh ./tools &&
     mv /.php-cs-fixer.dist.php .
+    set +x
 fi
 
 # # Pre commit hook
